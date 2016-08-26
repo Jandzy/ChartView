553d31e14ee07a4a39fc472f1c5c61eb58e9b031
jandzy <jandzy@163.com>
2016-08-26 17:18:44 +0800

    去掉全部测试代码，添加动画效果

--- src/main/java/com/example/chartview/MainActivity.java
+++ src/main/java/com/example/chartview/MainActivity.java
@@ -18,78 +18,13 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.e("create","create");
 
         setContentView(R.layout.activity_main);
         chartview = (ChartView) findViewById(R.id.chartview);
 
-        chartview.setValues(90,119,151);
-        chartview.setColors(Color.YELLOW,Color.GREEN,Color.BLACK);
-
-        chartview.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               /* final AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
-                alertDialog.setTitle("haah");
-                alertDialog.setMessage("message");
-                alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "确定", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        alertDialog.dismiss();
-                    }
-                });
-                alertDialog.show();*/
-
-                Intent intent = new Intent(MainActivity.this,TwoActivity.class);
-                startActivity(intent);
-
-        /*        PopupWindow popupWindow = new PopupWindow(500,500);
-                TextView textView = new TextView(MainActivity.this);
-                textView.setText("hahahahaha");
-                textView.setTextSize(20);
-                textView.setGravity(Gravity.CENTER);
-                textView.setBackgroundColor(Color.YELLOW);
-                popupWindow.setContentView(textView);
-                popupWindow.showAtLocation(chartview,10,20,20);*/
-            }
-        });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.e("onStrat","onstart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.e("resume","resume");
+        chartview.setValues(90, 119, 151,120);
+        chartview.setColors(Color.YELLOW, Color.GREEN);
 
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.e("pause","pause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.e("stop","stop");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.e("restart","restart");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.e("destroy","destroy");
-
-    }
 }
--- src/main/java/com/example/chartview/view/ChartView.java
+++ src/main/java/com/example/chartview/view/ChartView.java
@@ -17,33 +17,50 @@ import com.example.chartview.R;
 /**
  * Created by jrazy on 2016/8/25.
  */
-public class ChartView extends View implements ValueAnimator.AnimatorUpdateListener{
+public class ChartView extends View{
 
     //弧形
     public final static int ARC = 1;
     //圆形
     public final static int CHART = 0;
 
+    //半径
     private float mRadius;
+    /**
+     * mPieModel
+     *    enum  ARC 空心圆   CHART 实心圆
+     */
     private int mPieModel;
+
+    /**
+     * mShowAnim  是否显示动画 boolean
+     * 动画的原理是通过一点点改变drawArc的方法中sweepAngle的值，然后通过 postInvalidate()来重绘view实现。
+     */
     private boolean mShowAnim;
 
     //画笔
     private Paint mPaint;
     private RectF voal;
-    //起始位置
+
+    /**
+     * startPosition 画笔的起始位置，0的时候是三点钟位置。
+     */
     private float startPosition;
 
+    /**
+     * mValues 将传过来的值按照百分比分成valus.length长度存放（也就是分成几份的圆）
+     */
     private float[] mValues;
     private int[] mColors;
 
-    private Handler mHandler = new Handler();
-    private Runnable runnable = new Runnable() {
-        @Override
-        public void run() {
+    /**
+     * animValues 动态存放当前绘制了值
+     */
+    private float[] animValues;
+    private int mAnimCount;
 
-        }
-    };
+    private int count;//数组个数，几份
+
 
     public ChartView(Context context) {
         this(context,null);
@@ -55,7 +72,6 @@ public class ChartView extends View implements ValueAnimator.AnimatorUpdateListe
 
     public ChartView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        Log.e("chartview","chartview");
 
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ChartView);
         mRadius = typedArray.getDimension(R.styleable.ChartView_radius,40);
@@ -64,20 +80,14 @@ public class ChartView extends View implements ValueAnimator.AnimatorUpdateListe
         typedArray.recycle();
 
         mPaint = new Paint();
+        mPaint.setAntiAlias(true);//画笔消除锯齿
 
 
     }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        Log.e("measure","measure");
-    }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        Log.e("ondraw","ondraw");
         voal = new RectF();
         voal.left = getWidth()/2-mRadius;
         voal.top = getHeight()/2-mRadius;
@@ -86,7 +96,6 @@ public class ChartView extends View implements ValueAnimator.AnimatorUpdateListe
 
         //startPostition要在ondrew方法里面处事值，如果在构造方法中的话，activity在onrestart方法之后view会重绘调用ondraw方法。
         startPosition = 270;
-
 
 
         for(int i=0;i<mValues.length;i++){
@@ -105,33 +114,62 @@ public class ChartView extends View implements ValueAnimator.AnimatorUpdateListe
 
     }
 
+    public void setColors(int... colors){
+        mColors = colors;
+    }
+
     public void setValues(float... values){
         double sum = 0;
         mValues = new float[values.length];
         for(float value: values){
             sum += value;
         }
+        mAnimCount = values.length;
         for(int i=0; i<values.length; i++){
             mValues[i] =(float)(values[i]/sum)*360;
         }
-
-    }
-
-    public void setColors(int... colors){
-        mColors = colors;
-    }
-
-    @Override
-    public void onAnimationUpdate(ValueAnimator animation) {
-        float sum=0;
-        for(float value:mValues){
-            sum=value;
-            value=0;
-            if(value<sum){
-                value +=1;
-                invalidate();
-            }
-
+        if(mShowAnim){
+            count = 0;
+            animValues = mValues;
+            mValues = new float[mValues.length];
+            mHandler.removeCallbacks(runnable);
+            mHandler.post(runnable);
         }
+
     }
+
+    private Handler mHandler = new Handler();
+
+    /**
+     * 为了动画效果，需要先确定mValues的length，然后逐个赋值，先给mValues[0]赋值，如果mValues[0] 等于对应的百分比之后
+     * 再给mValues[1]赋值，以此类推。
+     * 赋值完需要return,否则循环赋值.
+     */
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            if(count <mAnimCount){
+                if(mValues[count]+1>animValues[count]){
+                    mValues[count] = animValues[count];
+                    if(count==mAnimCount-1){//结束
+                        return;
+                    }else{
+                        count++;
+                    }
+                }
+                if(mValues[count]<animValues[count]){
+                    mValues[count]++;
+                    postInvalidate();
+                    mHandler.postDelayed(this,1);
+                }
+            }else{
+                try {
+                    this.finalize();
+                } catch (Throwable throwable) {
+                    throwable.printStackTrace();
+                }
+            }
+        }
+    };
+
 }
